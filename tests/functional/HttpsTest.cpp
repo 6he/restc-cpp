cd23673ca87c87fa64a406d3499f8c284c23e5c8
Jarle Aase <jarle@jgaa.com>
2022-05-29 18:33:27 +0300

    Treoubleshooting tls connect test errorf on ubuntu Xenial and Debian Stretch

--- tests/functional/HttpsTest.cpp
+++ tests/functional/HttpsTest.cpp
@@ -50,12 +50,18 @@ const lest::test specification[] = {
 
 TEST(TestHTTPS)
 {
-    shared_ptr<boost::asio::ssl::context> tls_ctx = make_shared<boost::asio::ssl::context>(boost::asio::ssl::context{ boost::asio::ssl::context::sslv23 });
-    tls_ctx->set_options(boost::asio::ssl::context::default_workarounds
+    shared_ptr<boost::asio::ssl::context> tls_ctx = make_shared<boost::asio::ssl::context>(boost::asio::ssl::context{ boost::asio::ssl::context::tlsv12_client});
+
+    try {
+        tls_ctx->set_options(boost::asio::ssl::context::default_workarounds
                         | boost::asio::ssl::context::no_sslv2
                         | boost::asio::ssl::context::no_sslv3
-                        | boost::asio::ssl::context::no_tlsv1_1
+//                        | boost::asio::ssl::context::no_tlsv1_1
                         | boost::asio::ssl::context::single_dh_use);
+        } catch (const exception& ex) {
+           cout << " *** exception " << ex.what() << endl;
+           throw;
+        }
 
         auto client = RestClient::Create(tls_ctx);
         client->ProcessWithPromise([&](Context& ctx) {
